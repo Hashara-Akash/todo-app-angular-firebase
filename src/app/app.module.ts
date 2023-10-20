e8f94ed687907bca1e7fbd8838737c1151a2767f
Hashara-Akash <g10022.hashara@gmail.com>
2023-10-20 17:54:29 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AngularFireModule} from '@angular/fire/compat'
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore'
 
 import { AppComponent } from './app.component';
+import {environment} from 'src/environments/environment';
+import { ToDoComponent } from './to-do/to-do.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment. firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyC1mQm2t8nz9GFrMPkIVlyKuhhoTWFhcmk",
+    authDomain: "todo-list-2a8c0.firebaseapp.com",
+    projectId: "todo-list-2a8c0",
+    storageBucket: "todo-list-2a8c0.appspot.com",
+    messagingSenderId: "347843721297",
+    appId: "1:347843721297:web:afff6ca4712bb2a9796070"
+  }
 };
